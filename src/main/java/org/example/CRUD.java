077ab657c2a93c75bb8241add1657f0a1bf11772
dnldl <dnldlscjf@gmail.com>
2024-01-08 16:18:46 +0900

    CRUD 파일 읽는 방식으로 바꾸어 주기

--- src/main/java/org/example/CRUD.java
+++ src/main/java/org/example/CRUD.java
@@ -17,7 +17,7 @@ private List<Movie> list;
     public CRUD(){
         this.list = new ArrayList<>();
     }
-    public void printMenu() {
+    public String printMenu() throws IOException {
         System.out.println("=============[ 보고 싶은 영화 ] ==========");
         System.out.println("1. 영화 추가");
         System.out.println("2. 영화 수정");
@@ -25,126 +25,108 @@ private List<Movie> list;
         System.out.println("4. 영화 리스트 보기");
         System.out.println("5. 영화 검색");
         System.out.println("6. 영화 별점 순위");
+        System.out.println("7. 파일 저장");
         System.out.println("0. 프로그램 종료");
         System.out.println("=========================================");
-    }
-    public boolean menuChoose(String input){
-        switch (input) {
-                case "1":
-                    addData();
-                    break;
-                case "2":
-                    editData();
-                    break;
-                case "3":
-                    deleteData();
-                    break;
-                case "4":
-                    readData();
-                    break;
-                case "5":
-                    searchData();
-                    break;
-                case "6":
-                    topRanking();
-                    break;
-                case "0":
-                    System.out.println("프로그램을 종료 합니다!");
-                    return false;
-                default:
-                    System.out.println("잘못된 입력 입니다.");
-            }
-       return true;
+        br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
     }
 
-    private void addData() {
-        Movie m = new Movie();
-        try{
+
+    public Movie addData(ArrayList<Movie> list) throws IOException {
+        int num;
+        String name; //영화 이름
+        String genre;// 장르
+        int runningTime; //상영 시간
+        int viewor;// 누적 관객수
+        String regDate;// 기록 날짜
+        double rating_Point;//영화 평점
+
+        num = list.size();
+
             System.out.println("영화 제목을 입력하세요");
             br = new BufferedReader(new InputStreamReader(System.in));
-            m.setName(br.readLine());
+            name = br.readLine();
             System.out.println("영화 장르를 입력하세요 ex)드라마, 공포, 액션, 코미디");
-            m.setGenre(br.readLine());
+            genre=br.readLine();
             System.out.println("영화 상영 시간을 입력하세요 ex)분으로만 입력");
-            m.setRunningTime(Integer.parseInt(br.readLine()));
+            runningTime =Integer.parseInt(br.readLine());
             System.out.println("누적 관객 수를 입력하세요 *단위는 만 입니다.*  M");
-            m.setViewor(Integer.parseInt(br.readLine()));
+            viewor=Integer.parseInt(br.readLine());
             System.out.println("영화의 평점을 입력하세요 (10점 만점)");
-            m.setRating_Point(Float.parseFloat(br.readLine()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        m.setNum(this.list.size());
-        LocalDate date = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String regDate = date.format(formatter);
-        m.setRegDate(regDate);
 
-        this.list.add(m);
-        System.out.println("추가되었습니다.");
+            rating_Point=Double.parseDouble(br.readLine());
+            LocalDate date = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            regDate = date.format(formatter);
+
+            Movie m = new Movie(num,name,genre,runningTime,viewor, regDate, rating_Point);
+
+             return m;
+
     }
 
-    private  void editData() {
+   public void editData(ArrayList<Movie> list) {
         // Placeholder implementation
-       if(this.list.isEmpty()){
+       if(list.isEmpty()){
            System.out.print("데이터가 존재하지 않습니다");
            return;
        }
        try{
-           readData();
            System.out.println("수정할 번호 입력");
            br = new BufferedReader(new InputStreamReader(System.in));
            int num = Integer.parseInt(br.readLine()) -1;
-           if(valid(num)){
+           if(Main.valid(list , num)){
                System.out.println("영화 제목을 입력하세요");
                br = new BufferedReader(new InputStreamReader(System.in));
-               this.list.get(num).setName(br.readLine());
+               list.get(num).setName(br.readLine());
                System.out.println("영화 장르를 입력하세요 ex)드라마, 공포, 액션, 코미디");
-               this.list.get(num).setGenre(br.readLine());
+               list.get(num).setGenre(br.readLine());
                System.out.println("영화 상영 시간을 입력하세요 ex)분으로만 입력");
-               this.list.get(num).setRunningTime(Integer.parseInt(br.readLine()));
+               list.get(num).setRunningTime(Integer.parseInt(br.readLine()));
                System.out.println("누적 관객 수를 입력하세요 *단위는 만 입니다.*  M");
-               this.list.get(num).setViewor(Integer.parseInt(br.readLine()));
+               list.get(num).setViewor(Integer.parseInt(br.readLine()));
                System.out.println("영화의 평점을 입력하세요 (10점 만점)");
-               this.list.get(num).setRating_Point(Float.parseFloat(br.readLine()));
+               list.get(num).setRating_Point(Float.parseFloat(br.readLine()));
                LocalDate date = LocalDate.now();
                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
                String regDate = date.format(formatter);
-               this.list.get(num).setRegDate(regDate);
+               list.get(num).setRegDate(regDate);
                System.out.println("수정 되었습니다!");
            }else{
-               editData();
+               editData(list);
            }
        } catch (IOException e) {
            e.printStackTrace();
        }
     }
 
-    private  void deleteData() {
-        if(this.list.isEmpty()){
+    public void deleteData(ArrayList<Movie> list) {
+        if(list.isEmpty()){
             System.out.println("데이터가 존재하지 않았습니다!");
             return;
         }
        try{
-           readData();
+
            System.out.println("삭제할 영화 번호를 입력하세요");
            br = new BufferedReader(new InputStreamReader(System.in));
            int num = Integer.parseInt(br.readLine())-1;
-           if(valid(num)) {
+           if(Main.valid(list,num)) {
                this.list.remove(num);
                for (int i = 0; i < list.size(); i++) {
                    this.list.get(i).setNum(i);
                }
                System.out.println("삭제되었습니다.");
            }else{
-               deleteData();
+               deleteData(list);
            }
        }catch (IOException e){
            e.printStackTrace();
        }
     }
 
-    private void readData() {
+    public void readData(ArrayList<Movie> list) {
+
         if (list.isEmpty()) {
             System.out.println("데이터가 존재하지 않습니다.");
             return;
@@ -156,8 +138,8 @@ private List<Movie> list;
         }
     }
 
-    private void searchData() {
-        if (this.list.isEmpty()) {
+    public void searchData(ArrayList<Movie> list) {
+        if (list.isEmpty()) {
             System.out.println("데이터가 존재하지 않습니다");
             return;
         }
@@ -221,7 +203,7 @@ private List<Movie> list;
         }
 
 
-    private void topRanking() {
+   public void topRanking(ArrayList<Movie> list) {
         if (list.isEmpty()) {
             System.out.println("데이터가 존재하지 않습니다.");
             return;
@@ -239,13 +221,5 @@ private List<Movie> list;
         Collections.sort(list, Comparator.comparing(Movie::getNum));
     }
 
-    private boolean valid(int num) {
-        // 배열 범위
-        if (this.list.size() <= num || num == -1) {
-            System.out.println("없는 번호 입니다.");
-            return false;
-        }
 
-        return true;
-    }
 }
