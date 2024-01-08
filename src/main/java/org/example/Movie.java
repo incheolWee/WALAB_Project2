1f052fa5ac5649eb9444d0a9cd63edf8d2ad9446
dnldl <dnldlscjf@gmail.com>
2024-01-08 16:19:03 +0900

    lombok 라이브러리 사용하여 코드 간편화

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,90 +1,40 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class Movie {
     private int num; //순서
-    private  String name; //영화 이름
+    private String name; //영화 이름
     private String genre;// 장르
     private int runningTime; //상영 시간
     private int viewor;// 누적 관객수
     private String regDate;// 기록 날짜
     private double rating_Point;//영화 평점
+
     public Movie() {
     }
-    public Movie(int num, String name, String genre, int runningTime, int viewor,double rating_Point,String regDate){
+
+    public Movie(int num, String name, String genre, int runningTime, int viewor, String regDate, double rating_Point) {
         this.num = num;
         this.name = name;
         this.genre = genre;
         this.runningTime = runningTime;
         this.viewor = viewor;
-        this. rating_Point= rating_Point;
-        this.regDate= regDate;
+        this.rating_Point = rating_Point;
+        this.regDate = regDate;
     }
 
     @Override
     public String toString() {
-        return String.format("%2d",(num+1)) +"\t"
-                + String.format("%-7s",name) +"\t"+
-                String.format("%5s",genre) +"\t\t" +
-                String.format("%4d",runningTime)+"m" +"\t\t"+
-                String.format("%4d",viewor)+"M"+"\t\t"+
-                regDate+"\t\t"+
-                String.format("%.1f",rating_Point);
+        return String.format("%2d", (num + 1)) + "\t"
+                + String.format("%-7s", name) + "\t" +
+                String.format("%5s", genre) + "\t\t" +
+                String.format("%4d", runningTime) + "m" + "\t\t" +
+                String.format("%4d", viewor) + "M" + "\t\t" +
+                regDate + "\t\t" +
+                String.format("%.1f", rating_Point);
     }
 
-    public int getNum() {
-        return num;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getRunningTime() {
-        return runningTime;
-    }
-
-    public void setRunningTime(int runningTime) {
-        this.runningTime = runningTime;
-    }
-
-    public int getViewor() {
-        return viewor;
-    }
-
-    public void setViewor(int viewor) {
-        this.viewor = viewor;
-    }
-
-
-    public String getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(String regDate) {
-        this.regDate = regDate;
-    }
-
-    public double getRating_Point() {
-        return rating_Point;
-    }
-
-    public void setRating_Point(double rating_Point) {
-        this.rating_Point = rating_Point;
-    }
 }
